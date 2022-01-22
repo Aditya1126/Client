bcd426ba5aa072162d81c3ff1bc38b895ee0325a
Aditya1126 <aditya26.bhl@gmail.com>
2022-01-23 00:06:50 +0530

    added some more code and do some changes in package file

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -1,27 +1,66 @@
-import { useState, useEffect } from "react";
-import { Product } from "../../app/models/Product";
+import { Grid, Paper } from "@mui/material";
+import AppPagination from "../../app/components/AppPagination";
+import CheckboxButtons from "../../app/components/CheckboxButtons";
+import RadioButtonGroup from "../../app/components/RadioButtonGroup";
+import useProducts from "../../app/hooks/useProducts";
+import LoadingComponent from "../../app/layout/LoadingComponent";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { setPageNumber, setProductParams } from "./catalogSlice";
 import ProductList from "./ProductList";
+import ProductSearch from "./ProductSearch";
 
+const sortOptions = [
+    { value: 'name', label: 'Alphabetical' },
+    { value: 'priceDesc', label: 'Price - High to low' },
+    { value: 'price', label: 'Price - Low to high' },
+]
 
+export default function Catalog() {
+    const {products, brands, types, filtersLoaded, metaData} = useProducts();
+    const { productParams,  } = useAppSelector(state => state.catalog);
+    const dispatch = useAppDispatch();
 
-export default function Catalog(){
-
-
-    const [products, setProduct] = useState<Product[]>([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5000/api/products")
-        .then((res) => res.json())
-        .then((data) => setProduct(data));
-}, []);
-
-
-
+    if (!filtersLoaded) return <LoadingComponent message='Loading products...' />
 
     return (
-        <>
-        <ProductList products={products}/>
-    
-        </>
+        <Grid container columnSpacing={4}>
+            <Grid item xs={3}>
+                <Paper sx={{ mb: 2 }}>
+                    <ProductSearch />
+                </Paper>
+                <Paper sx={{ mb: 2, p: 2 }}>
+                    <RadioButtonGroup
+                        selectedValue={productParams.orderBy}
+                        options={sortOptions}
+                        onChange={(e) => dispatch(setProductParams({ orderBy: e.target.value }))}
+                    />
+                </Paper>
+                <Paper sx={{ mb: 2, p: 2 }}>
+                    <CheckboxButtons
+                        items={brands}
+                        checked={productParams.brands}
+                        onChange={(items: string[]) => dispatch(setProductParams({ brands: items }))}
+                    />
+                </Paper>
+                <Paper sx={{ mb: 2, p: 2 }}>
+                    <CheckboxButtons
+                        items={types}
+                        checked={productParams.types}
+                        onChange={(items: string[]) => dispatch(setProductParams({ types: items }))}
+                    />
+                </Paper>
+            </Grid>
+            <Grid item xs={9}>
+                <ProductList products={products} />
+            </Grid>
+            <Grid item xs={3} />
+            <Grid item xs={9} sx={{mb: 2}}>
+                {metaData &&
+                <AppPagination 
+                    metaData={metaData}
+                    onPageChange={(page: number) => dispatch(setPageNumber({pageNumber: page}))}
+                />}
+            </Grid>
+        </Grid>
     )
 }
--- src/features/catalog/ProductCard.tsx
+++ src/features/catalog/ProductCard.tsx
@@ -1,40 +1,54 @@
-import { Avatar, Button, Card, CardActions, CardContent, CardMedia, Typography, CardHeader } from "@mui/material";
-import { Product } from "../../app/models/Product";
+import { LoadingButton } from "@mui/lab";
+import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { Product } from "../../app/models/product";
+import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
+import { currencyFormat } from "../../app/util/util";
+import { addBasketItemAsync } from "../basket/basketSlice";
 
-interface Props{
-    product: Product;
+interface Props {
+    product: Product
 }
 
-export default function ProductCard({product}:Props){
+export default function ProductCard({ product }: Props) {
+    const {status} = useAppSelector(state => state.basket);
+    const dispatch = useAppDispatch();
+
     return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardHeader avatar={
-                    <Avatar sx={{ bgcolor: "secondary.main" }}>
+        <Card>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: 'secondary.main' }}>
                         {product.name.charAt(0).toUpperCase()}
                     </Avatar>
                 }
                 title={product.name}
                 titleTypographyProps={{
-                    sx: { fontWeight: "bold", color: "primary.main" },
+                    sx: { fontWeight: 'bold', color: 'primary.main' }
                 }}
-                />
-        <CardMedia
-          sx={{ height: 140, backgroundSize: "contain", bgcolor: 'primary.light' }}
-          image={product.pictureUrl}
-          title={product.name}
-        />
-        <CardContent>
-                <Typography gutterBottom color="secondary" variant="h5">
-                    ${(product.price/100).toFixed(2)}
+            />
+            <CardMedia
+                sx={{ height: 140, backgroundSize: 'contain', bgcolor: 'primary.light' }}
+                image={product.pictureUrl}
+                title={product.name}
+            />
+            <CardContent>
+                <Typography gutterBottom color='secondary' variant="h5">
+                    {currencyFormat(product.price)}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                    {product.brand}/{product.type}
+                    {product.brand} / {product.type}
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Add to cart</Button>
-                <Button size="small">View</Button>
+                <LoadingButton
+                    loading={status.includes('pendingAddItem' + product.id)}
+                    onClick={() => dispatch(addBasketItemAsync({productId: product.id}))}
+                    size="small">
+                    Add to cart
+                </LoadingButton>
+                <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
             </CardActions>
-      </Card>
+        </Card>
     )
 }
